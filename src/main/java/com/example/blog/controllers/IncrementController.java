1784db320bc985d401024106b206ef56806f4e6c
Charles Brant-Stec <charlesbrantstec@gmail.com>
2020-10-05 10:49:09 -0500

    created the project and completed the controllers lecture

---
+++ src/main/java/com/example/blog/controllers/IncrementController.java
@@ -0,0 +1,16 @@
+
+package com.example.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class IncrementController {
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number){
+        return number + " plus one is " + (number + 1) + "!";
+    }
+
+}
