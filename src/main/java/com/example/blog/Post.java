3d1fe59f06527c8d6057fe49beddf492352f8857
Charles Brant-Stec <charlesbrantstec@gmail.com>
2020-10-06 09:59:26 -0500

    roll-dice exercise done

---
+++ src/main/java/com/example/blog/Post.java
@@ -0,0 +1,9 @@
+package com.example.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class Post {
+
+}
---
+++ src/main/java/com/example/blog/RollDiceController.java
@@ -0,0 +1,30 @@
+package com.example.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class RollDiceController {
+
+    @GetMapping ("/roll-dice")
+    public String viewRollDice(){
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String guessedNumberResult(@PathVariable int n, Model model){
+        String message ="";
+        int rolledNum = (int) Math.random()*6 +1;
+        String roll = String.valueOf(rolledNum);
+        String guessedNum = String.valueOf(n);
+
+        if(roll.equals(guessedNum)) {
+            message = "You guessed the correct number:" + n + "";
+        }else{
+            message = "You guessed the wrong number";
+        }
+        model.addAttribute("message", message);
+        return "roll-dice";
+    }
+}
